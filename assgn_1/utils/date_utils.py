3d4e6da3229b90fcf4310c59a556043eb35d970a
Ben <rs_ben@163.com>
2026-05-17 22:08:14 +0800

    feat: add data and utils

---
+++ assgn_1/utils/date_utils.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+
+def generate_first_of_month_dates(start_date_str, end_date_str):
+    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
+
+    dates = []
+    current_date = datetime(start_date.year, start_date.month, 1)
+
+    while current_date <= end_date:
+        dates.append(current_date.strftime("%Y-%m-%d"))
+        if current_date.month == 12:
+            current_date = datetime(current_date.year + 1, 1, 1)
+        else:
+            current_date = datetime(current_date.year, current_date.month + 1, 1)
+
+    return dates
